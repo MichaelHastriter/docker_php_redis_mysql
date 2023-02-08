2ec9e53cf6a8eec5ec310312f178c4e2fc048bef
michael.hastriter <michaelhastriter@gmail.com>
2023-02-07 20:16:12 -0500

    Update index.php
    all good

--- HomePage/index.php
+++ HomePage/index.php
@@ -1,4 +1,21 @@
 <?php
+
+//pull movies from database
+
+
+{
+
+movie_id= "antman",
+movie_img="frozen.jpg",
+movie_title="Antman",
+movie_description="This is the description",
+... 
+... 
+
+}
+
+
+
 $english_movies = array(
     "1" => "images/frozen.jpg",
     "2" => "images/F1.jpg",
